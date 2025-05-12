4a9dbb2bd534d1f5af14c137b2def74349727988
SiddharthKarmokar <siddkarmokar@gmail.com>
2025-05-12 21:39:30 +0530

    updated response json

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 from uvicorn import run as app_run
-from src.speaksnap.pipeline.summarizer import router as summary_rounter
+from src.speaksnap.pipeline.summarizer import router as summary_router
 
 app = FastAPI()
 @app.get("/", tags=["authentication"])
 async def index():
     return RedirectResponse(url="/docs")
 
-app.include_router(summary_rounter, prefix="/api/summary")
+app.include_router(summary_router, prefix="/api/summary")
 
 if __name__ == "__main__":
     app_run("main:app", host="0.0.0.0", port=8000, reload=True)
